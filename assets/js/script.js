28fcd890c8272d15999c9f3f33d7fde6a594eebb
Prabhat Singh <prabhats1410@gmail.com>
2026-03-16 00:22:02 +0530

    contact-us and protfolio page complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-﻿document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
   const navbar = document.querySelector(".navbar");
   if (!navbar) return;
 
